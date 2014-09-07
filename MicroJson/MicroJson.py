1bf20839e089eb5df36b29338c3b372ebfc37cec
Roland Kruggel <rkruggel@bbf7.de>
2014-09-07 21:47:52 +0200

    diverse modification

--- MicroJson/MicroJson.py
+++ MicroJson/MicroJson.py
@@ -24,174 +24,6 @@ JSONDB_PREFIX = 'jsondb_'
 JSONDB_PATH = 'jsondb'
 
 
-class pickleDb(object):
-    loco = None  # der filename
-    fsave = None  # True -> es soll gespeichert werden
-    db = {}  # die DB als dict
-
-    def __init__(self, location, option):
-        """Creates a database object and loads the data from the location path.
-        If the file does not exist it will be created on the first update."""
-        self.load(location, option)
-
-    def load(self, location, option):
-        """Loads, reloads or changes the path to the db file.
-        DO NOT USE this method has it may be deprecated in the future."""
-        location = os.path.expanduser(location)
-        self.loco = location
-        self.fsave = option
-        if os.path.exists(location):
-            self._loaddb()
-        else:
-            self.db = {}
-        return True
-
-    def dump(self):
-        """Force save memory db to file."""
-        self._dumpdb(True)
-        return True
-
-    def set(self, key, value):
-        """Set the (string,int,whatever) value of a key"""
-        self.db[key] = value
-        self._dumpdb(self.fsave)
-        return True
-
-    def get(self, key):
-        """Get the value of a key"""
-        try:
-            return self.db[key]
-        except KeyError:
-            return None
-
-    def getall(self):
-        """Return a list of all keys in db"""
-        return self.db.keys()
-
-    def rem(self, key):
-        """Delete a key"""
-        del self.db[key]
-        self._dumpdb(self.fsave)
-        return True
-
-    def lcreate(self, name):
-        """Create a list"""
-        self.db[name] = []
-        self._dumpdb(self.fsave)
-        return True
-
-    def ladd(self, name, value):
-        """Add a value to a list"""
-        self.db[name].append(value)
-        self._dumpdb(self.fsave)
-        return True
-
-    def lgetall(self, name):
-        """Return all values in a list"""
-        return self.db[name]
-
-    def lget(self, name, pos):
-        """Return one value in a list"""
-        return self.db[name][pos]
-
-    def lrem(self, name):
-        """Remove a list and all of its values"""
-        number = len(self.db[name])
-        del self.db[name]
-        self._dumpdb(self.fsave)
-        return number
-
-    def lpop(self, name, pos):
-        """Remove one value in a list"""
-        value = self.db[name][pos]
-        del self.db[name][pos]
-        self._dumpdb(self.fsave)
-        return value
-
-    def llen(self, name):
-        """Returns the length of the list"""
-        return len(self.db[name])
-
-    def append(self, key, more):
-        """Add more to a key's value"""
-        tmp = self.db[key]
-        self.db[key] = ('%s%s' % (tmp, more))
-        self._dumpdb(self.fsave)
-        return True
-
-    def lappend(self, name, pos, more):
-        """Add more to a value in a list"""
-        tmp = self.db[name][pos]
-        self.db[name][pos] = ('%s%s' % (tmp, more))
-        self._dumpdb(self.fsave)
-        return True
-
-    def dcreate(self, name):
-        """Create a dict"""
-        self.db[name] = {}
-        self._dumpdb(self.fsave)
-        return True
-
-    def dadd(self, name, pair):
-        """Add a key-value pair to a dict, "pair" is a tuple"""
-        self.db[name][pair[0]] = pair[1]
-        self._dumpdb(self.fsave)
-        return True
-
-    def dget(self, name, key):
-        """Return the value for a key in a dict"""
-        return self.db[name][key]
-
-    def dgetall(self, name):
-        """Return all key-value pairs from a dict"""
-        return self.db[name]
-
-    def drem(self, name):
-        """Remove a dict and all of its pairs"""
-        del self.db[name]
-        self._dumpdb(self.fsave)
-        return True
-
-    def dpop(self, name, key):
-        """Remove one key-value in a dict"""
-        value = self.db[name][key]
-        del self.db[name][key]
-        self._dumpdb(self.fsave)
-        return value
-
-    def dkeys(self, name):
-        """Return all the keys for a dict"""
-        return self.db[name].keys()
-
-    def dvals(self, name):
-        """Return all the values for a dict"""
-        return self.db[name].values()
-
-    def dexists(self, name, key):
-        """Determine if a key exists or not"""
-        if self.db[name][key] is not None:
-            return 1
-        else:
-            return 0
-
-    def deldb(self):
-        """Delete everything from the database"""
-        self.db = {}
-        self._dumpdb(self.fsave)
-        return True
-
-    def _loaddb(self):
-        """Load or reload the json info from the file"""
-        # self.db = simplejson.load(open(self.loco, 'rb'))
-        self.db = json.load(open(self.loco, 'rb'))
-
-    def _dumpdb(self, forced):
-        """Dump (write, save) the json dump into the file"""
-        if forced:
-            # simplejson.dump(self.db, open(self.loco, 'wb'))
-            json.dump(self.db, open(self.loco, 'wb'))
-
-
 class MicroJsonBasic(object):
     path = None  # nur der dirname
     file = None  # nur der filename
@@ -379,10 +211,21 @@ class MicroJson(MicroJsonBasic):
         except KeyError:
             return None
 
-    def getlist(self, xid, xlistid):
+    def getlist(self, xid, xlistid=None):
         """ Get one item of the list"""
+        if isinstance(xlistid, basestring):
+            d1 = self.get(xid)
+            for ii in d1:
+                if ii['id'] == xlistid:
+                    return ii
+                a=0
+            a = 0
+
         try:
-            return self.get(xid)[xlistid]
+            if xlistid is None:
+                return self.get(xid)
+            else:
+                return self.get(xid)[xlistid]
         except KeyError:
             return None
 
@@ -447,7 +290,7 @@ class MicroJson(MicroJsonBasic):
 # self._savedb(True)
 # self.load()
 #
-#     #
+# #
 #     # laden / speichern
 #     #
 #
--- MicroJson/tests/MicroJsonTest.py
+++ MicroJson/tests/MicroJsonTest.py
@@ -22,28 +22,21 @@ __author__ = 'rkruggel'
 
 
 class MicroJsonTest(unittest.TestCase):
-    DICT_PATH = '../.testdb'
-    DICT_FILE = 't_dict.json'
-    DICT_FULLPATH = DICT_PATH + '/' + DICT_FILE
-    DICT_LISTFILE = 't_list.json'
-    DICT_LISTFULLPATH = DICT_PATH + '/' + DICT_LISTFILE
+    # Verzeichniss in dem die Testdb geschrieben wird
+    TEST_PATH = '.testdb'
+    # Name der TestDb
+    TEST_DB = 'test.json'
+    TEST_DB_FULLPATH = TEST_PATH + '/' + TEST_DB
 
     def inst_dict(self, para=True):
         """Instanz erzeugen"""
-        tb = MicroJson(self.DICT_FULLPATH, para)
-        self.assertIsInstance(tb, MicroJson, msg="MicroJson (Dict) kann nicht instanziert werden")
-        return MicroJson(self.DICT_FULLPATH, para)
-
-    def inst_list(self, para=True):
-        """Instanz erzeugen"""
-        tl = MicroJson(self.DICT_LISTFULLPATH, para)
-        self.assertIsInstance(tl, MicroJson, msg="MicroJson (List) kann nicht instanziert werden")
-        return MicroJson(self.DICT_LISTFULLPATH, para)
+        tb = MicroJson(self.TEST_DB_FULLPATH, para)
+        self.assertIsInstance(tb, MicroJson, msg="MicroJson kann nicht instanziert werden")
+        return tb
 
     def setUp(self):
         """"""
         self.tb = self.inst_dict()
-        self.tl = self.inst_list()
         self.dt = time.time()
         self.dt100 = int(self.dt / 100)
 
@@ -51,177 +44,155 @@ class MicroJsonTest(unittest.TestCase):
         """"""
         pass
 
-    def test000(self):
+    #
+    # ÄNDERUNGEN
+    #
+
+
+    # inst_dict  -> inst
+    # inst_list enfällt
+
+    #
+    # TEST BEGIN
+    #
+
+    def test_000(self):
         """ Der erste Test ist eigentlich kein test. Er löscht die DB. In
-        diesem Fall das gesamte subdir
+            diesem Fall das gesamte subdir
         """
         try:
-            shutil.rmtree(self.DICT_PATH)
+            shutil.rmtree(self.TEST_PATH)
         except:
             pass
 
-    def test001_dict_makeinstanze(self):
+    def test_201_dict_makeinstanze(self):
         """ init dict
-        Eine ganz neue Instanz erzeugen """
+            Eine ganz neue Instanz erzeugen """
         self.tb = self.inst_dict(False)
-        self.assertTrue(os.path.exists(self.DICT_FULLPATH),
-                        msg="MicroJson (dict) kann nicht instanziert werden. File nicht da")
+        self.assertTrue(os.path.exists(self.TEST_DB_FULLPATH),
+                        msg="MicroJson kann nicht instanziert werden. File nicht da")
 
-    def test001_list_makeinstanze(self):
-        """ init list
-        Eine ganz neue Instanz erzeugen """
-        self.tb = self.inst_list(False)
-        self.assertTrue(os.path.exists(self.DICT_LISTFULLPATH),
-                        msg="MicroJson (list) kann nicht instanziert werden. File nicht da")
+    # obsolete
+    # def test_201_list_makeinstanze(self):
+    # """ init list
+    # Eine ganz neue Instanz erzeugen """
+    # self.tb = self.inst_list(False)
+    # self.assertTrue(os.path.exists(self.TEST_LISTFULLPATH),
+    #                     msg="MicroJson (list) kann nicht instanziert werden. File nicht da")
 
-
-    def test002_dict_makeinstanze(self):
+    def test_202_dict_makeinstanze(self):
         """ init dict
-        Eine Instanz erzeugen wenn sie schon da ist """
+            Eine Instanz erzeugen wenn sie schon da ist """
         self.tb = self.inst_dict(True)
-        self.assertTrue(os.path.exists(self.DICT_FULLPATH),
+        self.assertTrue(os.path.exists(self.TEST_DB_FULLPATH),
                         msg="MicroJson (dict) kann nicht zum zweiten mal instanziert werden. File nicht da")
-
-    def test002_list_makeinstanze(self):
-        """ init list
-        Eine Instanz erzeugen wenn sie schon da ist """
-        self.tb = self.inst_list(True)
-        self.assertTrue(os.path.exists(self.DICT_LISTFULLPATH),
-                        msg="MicroJson (list) kann nicht zum zweiten mal instanziert werden. File nicht da")
 
     #
     # diverse 1
     #
 
-    def test010_dict_id(self):
+    def test_210_dict_id(self):
         """ diverses
-        Id holen
+            Id holen
         """
         # dt = int(time.time() / 100)
         id = self.tb.getId()
         b = int(float(id) / 100)
         self.assertTrue(b == self.dt100)
 
-    def test010_list_id(self):
+    def test_211_dict_count(self):
         """ diverses
-        Id holen
-        """
-        # dt = int(time.time() / 100)
-        id = self.tl.getId()
-        b = int(float(id) / 100)
-        self.assertTrue(b == self.dt100)
-
-    def test011_dict_count(self):
-        """ diverses
-        Anzahl der vorhandenen Datensätze
+            Anzahl der vorhandenen Datensätze
         """
         dd = self.tb.count()
         self.assertTrue(dd == 0)
 
-    def test011_list_count(self):
+    def test_212_dict_exist(self):
         """ diverses
-        Anzahl der vorhandenen Datensätze
-        """
-        dd = self.tl.count()
-        self.assertTrue(dd == 0)
-
-    def test012_dict_exist(self):
-        """ diverses
-        Sind Datensätze vorhanden
+            Sind Datensätze vorhanden
         """
         dd = self.tb.exist()
         self.assertFalse(dd)
 
-    def test012_list_exist(self):
-        """ diverses
-        Sind Datensätze vorhanden
-        """
-        dd = self.tl.exist()
-        self.assertFalse(dd)
-
-
     #
-    # insert
+    # set
     #
 
     # -01a-
-    def test020_dict_insert(self):
-        """ insert
-        Insert im dict """
+    def test_220_set_dict(self):
+        """ set
+            Insert im dict (mit Id) """
         data = {'name': 'petra', 'alter': 54, 'id': '4710'}
         self.id2 = self.tb.set(data)
         assert '4710' == self.id2
 
-    # -01b-
-    def test020_list_insert(self):
-        """ insert
-        Insert in list """
-        data = {'tell': 'gewitter', 'tale': 'Es blitzt und donnert'}
-        nid = self.tl.setlist(data, xdictid='101', xlistid='20')
-        self.assertTrue(nid == '20')
-
-
     # -02a-
-    def test021_dict_insert(self):
-        """ insert
-        Insert im dict """
+    def test_221_set_dict(self):
+        """ set
+        Insert im dict (ohne Id) """
         data = {'name': 'roland', 'alter': 54}
         self.id1 = self.tb.set(data)
         assert str(self.dt100) in self.id1
 
-    def test021_list_insert(self):
-        """ insert
-        Insert im dict """
-        data = {'tell': 'sturm', 'tale': 'Es windet seer'}
-        nid = self.tl.setlist(data, xlistid='20')
-        self.assertTrue(nid == '20')
-
-
     # -03a-
-    def test022_dict_insert(self):
-        """ insert
+    def test_222_set_dict(self):
+        """ set
         Insert im dict """
         data = {'name': 'roland', 'alter': 54, 'id': '4711'}
         self.id2 = self.tb.set(data)
         assert '4711' == self.id2
 
-    def test022_list_insert(self):
-        """ insert
-        Insert im dict """
-        data = {'tell': 'regen', 'tale': 'it\'s raining cats and dogs'}
-        nid = self.tl.setlist(data, xlistid='20')
-        self.assertTrue(nid == '20')
-
-
     # -04a-
-    def test024_dict_insert(self):
-        """ insert
+    def test_224_set_dict(self):
+        """ set
         Insert im dict """
         data = {'name': 'roland', 'alter': 54, 'id': '4711'}
         self.id3 = self.tb.set(data, '4711b')
         assert '4711b' == self.id3
 
-    def test024_list_insert(self):
-        """ insert
-        Insert im dict """
-        data = {'tell': 'schnee', 'tale': 'weisser pflaum liegt auf dem zaunpfosten'}
-        nid = self.tl.setlist(data, xlistid='20')
+
+    # -01b-
+    def test_220_set_list(self):
+        """ set
+        Insert in list """
+        data = {'tell': 'gewitter', 'tale': 'Es blitzt und donnert'}
+        nid = self.tb.setlist(data, xdictid='101', xlistid='20')
         self.assertTrue(nid == '20')
 
+    def test_221_set_list(self):
+        """ set
+        Insert im dict """
+        data = {'tell': 'sturm', 'tale': 'Es windet seer'}
+        nid = self.tb.setlist(data, xlistid='20')
+        self.assertTrue(nid == '20')
+
+    def test_222_set_list(self):
+        """ set
+        Insert im dict """
+        data = {'tell': 'regen', 'tale': 'it\'s raining cats and dogs'}
+        nid = self.tb.setlist(data, xlistid='20')
+        self.assertTrue(nid == '20')
+
+
+    def test_224_set_list(self):
+        """ set
+        Insert im dict """
+        data = {'tell': 'schnee', 'tale': 'weisser pflaum liegt auf dem zaunpfosten'}
+        nid = self.tb.setlist(data, xlistid='20')
+        self.assertTrue(nid == '20')
 
     #
     # diverse 2
     #
 
-    # todo: hier gehts weiter
-    def test030_count(self):
+    def test_230_count(self):
         """ diverses
         Anzahl der vorhandenen Datensätze
         """
         dd = self.tb.count()
         self.assertTrue(dd > 0)
 
-    def test031_exist(self):
+    def test_231_exist(self):
         """ diverses
         Sind Datensätze vorhanden
         """
@@ -232,7 +203,7 @@ class MicroJsonTest(unittest.TestCase):
     # get
     #
 
-    def test040_get01(self):
+    def test_240_get01(self):
         """ get
         Ein vorhandener Datensatz """
         dd = self.tb.get('4711')
@@ -241,7 +212,7 @@ class MicroJsonTest(unittest.TestCase):
         assert dd['alter'] == 54
         assert dd['id'] == '4711'
 
-    def test041_get02(self):
+    def test_241_get02(self):
         """ get
         Einen nicht vorhandener Datensatz
         :return:
@@ -249,7 +220,7 @@ class MicroJsonTest(unittest.TestCase):
         dd = self.tb.get('515151515151515')
         self.assertIsNone(dd)
 
-    def test044_change(self):
+    def test_244_change(self):
         """ change
         daten ändern und wieder lesen """
         dd = self.tb.get('4711')
@@ -259,7 +230,7 @@ class MicroJsonTest(unittest.TestCase):
         nic = self.tb.set(dd, '4711')
         self.assertTrue(nic == '4711')
 
-    def test045_get03(self):
+    def test_245_get03(self):
         """ get
         Ein vorhandener Datensatz """
         dd = self.tb.get('4711')
@@ -268,7 +239,7 @@ class MicroJsonTest(unittest.TestCase):
         assert dd['alter'] == 60
         assert dd['id'] == '4711'
 
-    def test050_remove(self):
+    def test_250_remove(self):
         """ delete
         datensatz löschen """
         nic = self.tb.rem('4711b')
@@ -279,10 +250,76 @@ class MicroJsonTest(unittest.TestCase):
 
 
     #
+    # - getlist
+    #
+    def test_260_getlist(self):
+        """ getlist
+            liest das gesamte array der liste
+        """
+        dd = self.tb.getlist('20')
+        self.assertEqual(dd[2]['tell'], 'regen')
+
+    def test_261_getlist(self):
+        """ getlist
+            Eine Liste definiert mit Key('20') und Array-Pos(0)
+        """
+        dd = self.tb.getlist('20', 0)
+        self.assertEqual(dd['id'], '101')
+        self.assertEqual(dd['tell'], 'gewitter')
+        self.assertEqual(len(dd), 3, "Anzahl ")
+
+    def test_262_getlist(self):
+        """ getlist
+            mit key und array key
+        """
+        dd = self.tb.getlist('20', '101')
+        self.assertEqual(dd['id'], '101')
+        a=0
+
+    # def test_265_getlist(self):
+    #     """ getlist
+    #         Eine Liste definiert mit Key('20') und Array-Pos(12121220)
+    #         Array-Pos ist falsch
+    #     """
+    #     try:
+    #         dd = self.tb.getlist('20', 12121220)
+    #     except Exception as ex:
+    #         a=1
+    #     # self.ass
+    #     assert len(dd) == 3
+    # IndexError: list index out of range
+
+
+    #
+    # - getkeys
+    #
+
+    #
+    # - getall
+    #
+
+    #
+    # - getfirst
+    #
+
+    #
+    # - getlast
+    #
+
+    #
+    # - getlistlast
+    #
+
+
+
+
+
+
+    #
     # ende
     #
 
-    def test999(self):
+    def test_999(self):
         """
         Aufräumen
         """
